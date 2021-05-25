df8dba7c0eca140c0e52600459610bab0cd1d81d
Simon.Brandone <simon.brandone@utb.ecutbildning.se>
2021-05-25 16:41:58 +0200

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,12 @@ const Product = require('../modules/productsModel');
 const mongoose = require('mongoose');
 const db = mongoose.connection
 
-
+//Hämta produkter, fungerar
 router.get('/api/products', (req, res) => {
     Product.find({}, function(err, data) {res.json(data); console.log(err, data, data.length); });
 })
 
+//Lägga till produkt. Fungerar
 router.post('/api/products', (req, res) => {
     let newOrder = new Product({
         title: req.body.title,
@@ -26,11 +27,14 @@ router.post('/api/products', (req, res) => {
     }
 })
 
+
+//För att ta bort en. Funkar inte riktigt, kan deleta annat än det man klickar på.
 router.delete('/api/products/:_id', (req, res, next) => {
     Product.collection.deleteOne({}, function(err, data) {res.send(data); console.log(err, data, data.length); });
 })
 
+//för att uppdatera en order. Samma sak som med delete. ändrar inte den man klickar på alltid.
 router.patch('/api/products/:_id', (req, res, next) => {
-    
+
 })
 module.exports = router
