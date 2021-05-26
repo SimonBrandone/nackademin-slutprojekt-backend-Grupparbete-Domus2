28a38271e545d513edf5fb4451e953ca37d9f76f
Simon.Brandone <simon.brandone@utb.ecutbildning.se>
2021-05-26 15:11:55 +0200

    a

--- modules/ordersModel.js
+++ modules/ordersModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 
 const orderSchema = mongoose.Schema({
-    // _id: String,
+    _id: String,
     status: {
         type: String,
         default: 'In process'
@@ -24,5 +24,3 @@ const orderSchema = mongoose.Schema({
 const Order = mongoose.model('Order', orderSchema)
 
 module.exports = Order
-
-// { timestamps: true}
--- modules/productsModel.js
+++ modules/productsModel.js
@@ -12,25 +12,3 @@ const Product = mongoose.model('products',
                'products');
 
 module.exports = Product
-
-
-/* const orderSchema = mongoose.Schema({
-  _id: String,
-  title: {
-      type: String,
-  },
-  price: {
-      type: Number,
-  },
-  items: {
-      type: mongoose.Schema.Types.Array,
-      ref: 'Products'
-  },
-  orderValue: Number
-},
-
-) */
-
-// const Order = mongoose.model('Order', orderSchema)
-
-// module.exports = Order
